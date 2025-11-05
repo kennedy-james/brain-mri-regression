60b5967833a38c07aca2645bad927e91df33f514
Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>
2025-11-05 23:43:01 +0100

    feat: wip added generate outlier interim data
    Signed-off-by: Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>

--- scripts/generate_interim.py
+++ scripts/generate_interim.py
@@ -12,38 +12,54 @@ class InterimMode(Enum):
     feature_selection = auto()
 
 
-INTERIMMODE = InterimMode.imputation
+INTERIM_MODE = InterimMode.imputation
+
+IMP_FILENAME = f'X_interim_{configs['impute_method'].name}'
+match configs['impute_method']:
+    case Imputer.knn:
+        IMP_FILENAME += f'_{configs["knn_neighbours"]}n_{configs["knn_weight"]}w'
+    case Imputer.iterative:
+        IMP_FILENAME += f'_{configs['iterative_estimator'][:-2]}_{configs['iterative_iter']}iter'
+    case _:
+        raise NotImplementedError(
+            f"Imputation {configs['impute_method'].name} not implemented for interim data generation.")
 
 
-def generate_imputation():
+def generate_imputation(x):
     """ Uses imputation method set in configs.py """
-    print("Generating interim data...")
+    global IMP_FILENAME
+    print(f"Generating interim data from imputer {configs['impute_method'].name}...")
     x, _ = load_train_data()
     imputer = imputation(x, i=None)
     x_imp = imputer.transform(x)
-    filename = f'X_interim_{configs['impute_method'].name}'
+    filepath = INTERIM_DATA_DIR / IMP_FILENAME
+    interim_file = filepath.with_suffix('.csv')
+    print(f"Saving imputed data to {interim_file}...")
+    pd.DataFrame(x_imp).to_csv(interim_file, index=False, header=False)
 
-    if configs['impute_method'] == Imputer.knn:
-        filename += f'_{configs["knn_neighbours"]}n_{configs["knn_weight"]}w'
-    elif configs['impute_method'] == Imputer.iterative:
-        filename += f'_{configs['iterative_estimator'][:-2]}_{configs['iterative_iter']}iter'
 
+def generate_outlier(x):
+    """ Uses outlier method set in configs.py """
+    print(f"Generating outlier {configs['outlier_method'].name} data from configured imputer {configs['impute_method'].name}...")
+    x, _ = load_train_data()
+    imputer = imputation(x, i=None)
+    x_imp = imputer.transform(x)
     filepath = INTERIM_DATA_DIR / filename
     interim_file = filepath.with_suffix('.csv')
     print(f"Saving imputed data to {interim_file}...")
     pd.DataFrame(x_imp).to_csv(interim_file, index=False, header=False)
 
 
-def generate_outlier():
+def generate_feature_selection(x):
+    """ Uses feature_selection method set in configs.py """
+    print(f"Generating feature selected data from configured outlier {configs['outlier_method'].name} and {configs['impute_method'].name}...")
     pass
 
-
-def generate_feature_selection():
-    pass
+def main():
 
 
 if __name__ == "__main__":
-    match INTERIMMODE:
+    match INTERIM_MODE:
         case InterimMode.imputation: generate_imputation()
         case InterimMode.outlier: generate_outlier()
         case InterimMode.feature_selection: generate_feature_selection()
